2cdb3891b2cd3bdb58fe89fb1e1c387a3b94ecef
RuiBabosaJ <ruibarbosadev@gmail.com>
2025-10-29 11:27:28 -0300

    feat: Configuração inicial do projeto

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold underline">teste</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
